5c93413a1f4c6e0e01d6a3ecde4e3a0a3761e6ff
mjalemangarcia <mjalemangarcia@gmail.com>
2023-02-26 22:04:31 +0000

    Update HelloworldApplication.java

--- run/helloworld/src/main/java/com/example/helloworld/HelloworldApplication.java
+++ run/helloworld/src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -35,7 +35,7 @@ public class HelloworldApplication {
   class HelloworldController {
     @GetMapping("/")
     String hello() {
-      return "Hello " + name + "!";
+      return "Hello test " + name + "!";
     }
   }
 
